3fe2c8fa0b0822bf301ff0bcde9a54b974bf5b61
NikhilM512 <nikhil1995@gmail.com>
2026-01-19 14:41:01 +0530

    feat: set up initial project structure and http server

---
+++ src/index.js
@@ -0,0 +1,11 @@
+
+const http = require('http');
+
+
+let server = http.createServer((req,res)=>{
+    // console.log("Server is running on http://localhost:3000")
+    res.writeHead(200,{'content-type':'text/plain'});
+    res.end('Hello, World!');
+});
+
+server.listen(3000);
